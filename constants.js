b315477f0427a57809abcf5a79a174550575b01c
Eixix <tobiasbetz@hotmail.de>
2022-06-03 14:47:19 +0200

    Added color scales and other coloring methods

--- constants.js
+++ constants.js
@@ -1,6 +1,10 @@
 const SVG_MARGIN = { top: 20, right: 90, bottom: 20, left: 90 };
 const SVG_WIDTH = window.innerWidth - SVG_MARGIN.left - SVG_MARGIN.right;
-const SVG_HEIGHT = window.innerHeight - SVG_MARGIN.top - SVG_MARGIN.bottom - (window.innerHeight / 10); // To compensate for the footer
+const SVG_HEIGHT =
+  window.innerHeight -
+  SVG_MARGIN.top -
+  SVG_MARGIN.bottom -
+  window.innerHeight / 10; // To compensate for the footer
 
 const RECT_MARGIN = { right: 8, left: 8 };
 const RECT_HEIGHT = 35;
@@ -21,15 +25,28 @@ let nodeIdCounter = 0;
 const PSEUDO_NODE_SIZE = 20;
 
 let CONSTRAINT_HIGHLIGHT_COLORS = [
-    "aqua",
-    "blueviolet",
-    "chartreuse",
-    "crimson",
-    "darkorange",
-    "forestgreen",
-    "red",
-    "yellow",
-    "indigo",
+  "aqua",
+  "blueviolet",
+  "chartreuse",
+  "crimson",
+  "darkorange",
+  "forestgreen",
+  "red",
+  "yellow",
+  "indigo",
+];
+
+const COLORING_MAP = [
+  "#4e78b5",
+  "#6694c1",
+  "#80b1cc",
+  "#9dced6",
+  "#c0eade",
+  "#ffffe0",
+  "#eb6574",
+  "#d5405e",
+  "#b81b4a",
+  "#93003a",
 ];
 
 const STROKE_WIDTH_CONSTANT = 4;
--- navigationMenu.js
+++ navigationMenu.js
@@ -1,36 +1,81 @@
-
 let isColorCoded = false;
 
-document.querySelector('#navigation-menu-count').addEventListener('click', () => {
-    
+document
+  .querySelector("#navigation-menu-count")
+  .addEventListener("click", () => colorNodes(countNodes));
 
-    if (isColorCoded) {
-        for (const node of allNodes) {
-            node.data.color = node.data.isAbstract ? NODE_ABSTRACT_COLOR : NODE_COLOR;
-        }
-    } else {
-        const count = {};
-        let max = 0;
-        for (const node of allNodes) {
-            if (count[node.data.name]) {
-                count[node.data.name] += 1;
-                max = max < count[node.data.name] ? count[node.data.name] : max;
-            } else {
-                count[node.data.name] = 1;
-                max = max < count[node.data.name] ? count[node.data.name] : max;
-            }
-        }
-        
-        const colors = d3.scaleLinear().domain([1, max]).range(['white', 'green']);
+document
+  .querySelector("#navigation-menu-count-direct-children")
+  .addEventListener("click", () => colorNodes(countDirectChildren, "aqua"));
 
-        for (const node of allNodes) {
-            if (count[node.data.name] && !node.data.isAbstract) {
-                node.data.color = colors(count[node.data.name]);
-            }
-        }
+document
+  .querySelector("#navigation-menu-count-total-children")
+  .addEventListener("click", () => colorNodes(countTotalChildren, "orange"));
+
+function colorNodes(coloringFunction, color = "green") {
+  if (isColorCoded) {
+    for (const node of allNodes) {
+      node.data.color = node.data.isAbstract ? NODE_ABSTRACT_COLOR : NODE_COLOR;
     }
-    
-    isColorCoded = !isColorCoded;
+  } else {
+    const [count, max] = coloringFunction(); // Must return {"nodeName": integer}
+    const colors = d3
+      .scaleLinear()
+      .domain(d3.ticks(1, max, COLORING_MAP.length))
+      .range(COLORING_MAP);
 
-    updateSvg();
-});
+    for (const node of allNodes) {
+      if (count[node.data.name] !== undefined && !node.data.isAbstract) {
+        node.data.color = colors(count[node.data.name]);
+      }
+    }
+  }
+
+  isColorCoded = !isColorCoded;
+
+  updateSvg();
+}
+
+/**
+ * Counts all nodes
+ * @returns [{"nodeName": integer}, maxAmount]
+ */
+function countNodes() {
+  let count = {};
+  let max = 0;
+  for (const node of allNodes) {
+    if (count[node.data.name]) {
+      count[node.data.name] += 1;
+      max = max < count[node.data.name] ? count[node.data.name] : max;
+    } else {
+      count[node.data.name] = 1;
+      max = max < count[node.data.name] ? count[node.data.name] : max;
+    }
+  }
+
+  return [count, max];
+}
+
+function countDirectChildren() {
+  let count = {};
+  let max = 0;
+
+  for (const node of allNodes) {
+    count[node.data.name] = node.data.childrenCount();
+    max = max < count[node.data.name] ? count[node.data.name] : max;
+  }
+  console.log(count);
+  return [count, max];
+}
+
+function countTotalChildren() {
+  let count = {};
+  let max = 0;
+
+  for (const node of allNodes) {
+    count[node.data.name] = node.data.totalSubnodesCount();
+    max = max < count[node.data.name] ? count[node.data.name] : max;
+  }
+
+  return [count, max];
+}
